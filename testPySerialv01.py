bc7659b0033294bdeaa0e3c2cbf20f43a5ebf477
SanmicHuang <sanmic@mail.lit.edu.tw>
2017-06-02 10:06:39 +0800

    commit

---
+++ testPySerialv01.py
@@ -0,0 +1,18 @@
+# ============================================================================
+# Designer: Sanmic Huang
+# Site: httt://sanmic.lit.edu.tw
+# Date: 2017/05/25
+# Goal: Serial Communication
+# Docs: http://pythonhosted.org/pyserial/  http://pythonhosted.org/pyserial/
+# Version: 1.0.0
+# Description:
+# Note: ls /dev/tty.*
+# Run: Fn+F5 Comment:CMD+/ Terminal:^`
+# ============================================================================
+# encoding: utf-8
+
+import pyfirmata
+pin=13
+port='/dev/tty.usbmodem1421'
+board=pyfirmata.Arduino(port)
+board.digital[pin].write(1)
