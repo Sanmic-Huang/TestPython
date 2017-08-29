bc4f65639aa07bc02cbd0bb953f0017ed66e5d7c
SanmicHuang <sanmic@mail.lit.edu.tw>
2017-08-29 21:46:21 +0800

    submit

--- showPic.py
+++ showPic.py
@@ -9,7 +9,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # img = cv2.imread('Lena512BW.png', 0)
-img = cv2.imread('/Volumes/SanmicSD/0-Backup/Code/1-DB/Lena/BW/Lena512BW.png', 0)
+# img = cv2.imread('/Volumes/SanmicSD/0-Backup/Code/1-DB/Lena/BW/Lena512BW.png', 0)
+path='/Volumes/SanmicSD/0-Backup/Code/1-DB/Lena/BW/'
+filename='Lena512BW.png'
+img = cv2.imread(path+filename, 0)
 plt.imshow(img, cmap='gray', interpolation='bicubic')
 plt.xticks([]), plt.yticks([])
 plt.show()
