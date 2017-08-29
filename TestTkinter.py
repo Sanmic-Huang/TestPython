3d7b596295e72155115355932ac3d40cfa85f83c
SanmicHuang <sanmic@mail.lit.edu.tw>
2017-08-30 00:11:35 +0800

    submit

---
+++ TestTkinter.py
@@ -0,0 +1,14 @@
+# ===========================================
+# Designer: Sanmic Huang
+# Date: 2017/08/29
+# SOP: shift+CMD+D, fn+F5, cmd+W
+# GIT: shift+Ctl+G, Keyin submit, cmd+enter
+# Goal: Test tkinter package
+# ===========================================
+from Tkinter import *
+
+window=Tk()
+window.title("Sanmic GUI")
+window.geometry("300x200")
+window.maxsize(300,200)
+window.mainloop()
